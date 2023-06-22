90f15f2df9573e2ce331d5b782a59e67a8e7caa3
Fayez <fayezkhan680@gmail.com>
2023-06-22 23:59:50 +0530

    changes made

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,5 +44,6 @@ class ModelTrainer:
             }
             model_report :dict=evaluate_models(X_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,meodels=models)
             
+            best_model_score=max(sorted(model_report.values()))
         except:
             pass
