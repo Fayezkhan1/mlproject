f769f66f9f66925335fca912a6f885272c22905c
Fayez <fayezkhan680@gmail.com>
2023-06-23 23:06:50 +0530

    changes made

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,5 +45,11 @@ class ModelTrainer:
             model_report :dict=evaluate_models(X_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,meodels=models)
             
             best_model_score=max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
         except:
+
+
             pass
