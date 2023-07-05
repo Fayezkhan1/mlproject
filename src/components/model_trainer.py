1daec413d6e943e62aafc8e0b21c00c6558f6104
Fayez <fayezkhan680@gmail.com>
2023-07-05 23:15:35 +0530

    updated utils and model_trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -86,7 +86,38 @@ class ModelTrainer:
                 }
                 
             }
-        except:
+
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+        
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
-            pass
+
+            
